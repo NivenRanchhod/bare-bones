c76e2bc7f4098a95b5f55a88a184ba5a1643edf7
Niven Ranchhod <niven.r88@gmail.com>
2022-10-06 00:55:19 +0100

    Fixing styles not appearing

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(syntaxHighlight);
 
+  eleventyConfig.setUseGitIgnore(false);
+
   return {
     pathPrefix: "/bare-bones/"
   }
