d00ac9656e20abc2abb909729de85f28b19a18c7
Niven Ranchhod <niven.r88@gmail.com>
2022-10-12 18:04:01 +0100

    Removed web manifest

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("site.webmanifest");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
 
   eleventyConfig.addPlugin(syntaxHighlight);
 
